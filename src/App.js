73ec63c57ba98d26514ceff883838b475344725b
happi89 <farhaanp99@gmail.com>
2022-07-09 10:25:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import _ from 'lodash';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.id = _.uniqueId("prefix-");
+
+    this.state = {
+      countries: []
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://restcountries.com/v2/all')
+      .then(res => res.json())
+      .then(users => this.setState(() => {
+        return {countries: users}
+      }))
+  }
+
+  render() {
+    const id = this.id;
+
+    return (
+      <div className="App">
+        {this.state.countries.map((country) => {
+          return (
+            <div key={id}>
+              <h1>{country.name}</h1>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
