4473d7180396c80c9821a14f5ca948f8ad47dd40
happi89 <farhaanp99@gmail.com>
2022-07-09 14:35:13 -0400

    fixed mistake with name

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => (
 			return (
 				<div className={props.className} key={lodash.uniqueId('id_')}>
 					<img src={country.flags.svg} alt={`${country.name} flag`} />
-					<h1>{country.name.common}</h1>
+					<h1>{country.name}</h1>
 					<p>Population: {country.population}</p>
 					<p>Region: {country.region}</p>
 					<p>Capital: {country.capital}</p>
