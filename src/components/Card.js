3f90387c16238fa8e65240b1584f40c042548f96
happi89 <farhaanp99@gmail.com>
2022-08-26 01:18:14 -0400

    some small touches

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,37 @@
 import { Link } from 'react-router-dom';
 
-const Card = (props) => (
-	<div className='flex flex-wrap gap-12 justify-center'>
-		{props.countries.map((country) => {
-			return (
-				<div className='card w-96 bg-base-100 shadow-lg p-6' key={country.name}>
-					<Link to={`countries/${country.name}`}>
-						<img
-							className='h-52 w-80'
-							src={country.flags.svg}
-							alt={`${country.name} flag`}
-						/>
-						<h1 className='font-bold text-xl my-4'>{country.name}</h1>
-						<p>
-							<span className='font-bold'>Population:</span>{' '}
-							{country.apopulation}
-						</p>
-						<p>
-							<span className='font-bold'>Region:</span> {country.region}
-						</p>
-						<p>
-							<span className='font-bold'>Capital:</span> {country.capital}
-						</p>
-					</Link>
-				</div>
-			);
-		})}
-	</div>
-);
+const Card = (props) => {
+	const roundNum = (num) => {
+		if (num > 999) {
+			return Math.round(num / 100) / 10 + 'k';
+		}
+	};
+
+	return (
+		<div className='flex flex-wrap gap-12 justify-center'>
+			{props.countries.map((country) => {
+				const { name, flags, population, region, capital } = country;
+				return (
+					<div className='card w-96 bg-base-100 shadow-lg p-6' key={name}>
+						<Link to={`countries/${name}`}>
+							<img className='h-52 w-80' src={flags.svg} alt={`${name} flag`} />
+							<h1 className='font-bold text-xl my-4'>{name}</h1>
+							<p>
+								<span className='font-bold'>Population:</span>{' '}
+								{roundNum(population)}
+							</p>
+							<p>
+								<span className='font-bold'>Region:</span> {region}
+							</p>
+							<p>
+								<span className='font-bold'>Capital:</span> {capital}
+							</p>
+						</Link>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default Card;
--- src/components/stats.js
+++ src/components/stats.js
@@ -1,65 +1,77 @@
 import { Link } from 'react-router-dom';
 
 export function Stats({ country, borders }) {
+	const {
+		name,
+		nativeName,
+		population,
+		region,
+		subregion,
+		capital,
+		currencies,
+		languages,
+		topLevelDomain,
+	} = country;
 	return (
 		<div className='ml-32 mt-12'>
-			<h1 className='text-2xl font-bold mb-4'>{country?.name}</h1>
+			<h1 className='text-2xl font-bold mb-4'>{name}</h1>
 
 			<p>
-				<span className='font-bold'>Native Name:</span> {country?.nativeName}
+				<span className='font-bold'>Native Name:</span> {nativeName}
 			</p>
 			<p>
-				<span className='font-bold'>Population:</span> {country?.population}
+				<span className='font-bold'>Population:</span> {population}
 			</p>
 			<p>
-				<span className='font-bold'>Region:</span> {country?.region}
+				<span className='font-bold'>Region:</span> {region}
 			</p>
 			<p>
-				<span className='font-bold'>Sub Region:</span> {country?.subregion}
+				<span className='font-bold'>Sub Region:</span> {subregion}
 			</p>
 			<p>
-				<span className='font-bold'>Capital:</span> {country?.capital}
+				<span className='font-bold'>Capital:</span> {capital}
 			</p>
 			<p>
-				<span className='font-bold'>Top Level Domain:</span>{' '}
-				{country?.topLevelDomain}
+				<span className='font-bold'>Top Level Domain:</span> {topLevelDomain}
 			</p>
 			<p>
 				<span className='font-bold'>Currency:</span>{' '}
-				{country?.currencies?.length !== 1 ? (
-					country?.currencies?.map((c) => (
+				{currencies?.length !== 1 ? (
+					currencies?.map((c) => (
 						<span className='mx-1' key={c?.name}>
 							{c?.name},
 						</span>
 					))
 				) : (
-					<span>{country?.currencies[0]?.code}</span>
+					<span>{currencies[0]?.code}</span>
 				)}
 			</p>
 			<div>
 				<span className='font-bold'>Languages:</span>{' '}
-				{country?.languages?.length !== 1 ? (
-					country?.languages?.map((l) => (
+				{languages?.length !== 1 ? (
+					languages?.map((l) => (
 						<span className='mx-1' key={l?.name}>
 							{l?.name},
 						</span>
 					))
 				) : (
-					<span>{country?.languages[0]?.name}</span>
+					<span>{languages[0]?.name}</span>
 				)}
-				<div>
-					<p className='font-bold'>
-						Bordering Countries:{' '}
-						{borders?.map((b) => (
-							<Link
-								to={`/countries/${b.name}`}
-								className='mx-1 link'
-								key={b.name}>
-								{b.name},
-							</Link>
-						))}
-					</p>
-				</div>
+				{borders.length > 0 ? (
+					<div>
+						<p className='font-bold'>
+							Bordering Countries:{' '}
+							{borders?.map((b) => (
+								<Link
+									to={`/countries/${b.name}`}
+									className='mx-1 link'
+									key={b.name}>
+									{b.name},
+								</Link>
+							))}
+						</p>
+					</div>
+				) : null}
 			</div>
 		</div>
 	);
