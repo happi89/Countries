38f76e9ef0bc3b5454290f680993af2ed251ce22
happi89 <farhaanp99@gmail.com>
2022-08-26 01:33:04 -0400

    added all option in select

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,16 +5,22 @@ const SearchBox = ({
 	onSelectChange,
 	placeholder,
 	options,
-}) => (
-	<div className='mb-8 p-6 flex flex-wrap gap-x-8 gap-y-2 items-center justify-around'>
-		<input
-			className='input input-bordered w-full max-w-xs shadow-sm'
-			type='search'
-			placeholder={placeholder}
-			onChange={onSearchChange}
-		/>
-		<Select onChange={onSelectChange} options={options} />
-	</div>
-);
+}) => {
+	return (
+		<div className='mb-8 p-6 flex flex-wrap gap-x-8 gap-y-2 items-center justify-around'>
+			<input
+				className='input input-bordered w-full max-w-xs shadow-sm'
+				type='search'
+				placeholder={placeholder}
+				onChange={onSearchChange}
+			/>
+			<Select
+				onChange={onSelectChange}
+				options={options}
+				defaultValue={options[0]}
+			/>
+		</div>
+	);
+};
 
 export default SearchBox;
